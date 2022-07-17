478b0d2261e063befc458301e9fc2ac32ec7cb00
CarolFOliveira <107504746+CarolFOliveira@users.noreply.github.com>
2022-07-17 18:10:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function mostrar(id){
+   if(document.getElementById(id).style.display == 'none'){
+    document.getElementById(id).style.display = 'grid';
+    
+   }else{
+    document.getElementById(id).style.display = 'none';
+   }
+    
+ 
+        
+}
